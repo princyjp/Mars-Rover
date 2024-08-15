dcd0f31458f6722fae1f966d018c2a3c06c25914
princyjp <princy.philip@outlook.com>
2024-08-15 12:02:13 +0100

    InputLayer:feat: Create constructor for PlateauSize class and Position class

--- src/main/java/InputLayer/Position.java
+++ src/main/java/InputLayer/Position.java
@@ -4,4 +4,10 @@ public class Position {
     private int x;
     private int y;
     private CompassDirection facing;
+
+    public Position(int x, int y, CompassDirection facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+    }
 }
