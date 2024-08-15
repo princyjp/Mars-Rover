512ed21cdd0e3cba8111507a9130660a5e8dc69a
princyjp <princy.philip@outlook.com>
2024-08-15 15:24:35 +0100

    feat:InputLayer:InputParser:InstructionParser:Implement parseStringToInstruction method

--- src/main/java/InputLayer/InputParser/InstructionParser.java
+++ src/main/java/InputLayer/InputParser/InstructionParser.java
@@ -1,4 +1,43 @@
 package InputLayer.InputParser;
 
+import InputLayer.Instruction;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InstructionParser {
+    private String instructions;
+
+    public InstructionParser(String instructions) {
+        if(instructions != null || !instructions.isBlank() ) {
+            this.instructions = instructions.toUpperCase();
+        }else{
+            this.instructions = " ";
+        }
+    }
+    public Queue<Instruction> parseStringToInstruction(){
+        Queue<Instruction> instructionQueue = new LinkedList<>();
+
+        if(!instructions.isBlank()) {
+            for (char c : instructions.toCharArray()) {
+                switch (c) {
+                    case 'L':
+                        instructionQueue.add(Instruction.L);
+                        break;
+                    case 'R':
+                        instructionQueue.add(Instruction.R);
+                        break;
+                    case 'M':
+                        instructionQueue.add(Instruction.M);
+                        break;
+                    default:
+                        System.out.println(c + " is an invalid instruction.");
+                        break;
+
+                }
+            }
+        }
+        return instructionQueue;
+    }
 }
