12648a327275f5fc2e577dfbc35725a0878637f3
princyjp <princy.philip@outlook.com>
2024-08-16 09:09:20 +0100

    feat:InputLayer:InputParser: Add functionality to CompassDirectionParser class

--- src/main/java/InputLayer/InputParser/CompassDirectionParser.java
+++ src/main/java/InputLayer/InputParser/CompassDirectionParser.java
@@ -1,13 +1,49 @@
 package InputLayer.InputParser;
 
-public class CompassDirectionParser {
-    private final String compassDirection;
+import InputLayer.CompassDirection;
+import InputLayer.Instruction;
 
-    public CompassDirectionParser(String compassDirection) {
-        if (compassDirection != null && !compassDirection.isBlank()) {
-            this.compassDirection = compassDirection.toUpperCase();
+public class CompassDirectionParser {
+    private String input;
+    CompassDirection compassDirection;
+
+    public CompassDirectionParser(String input) {
+        this.input = input;
+        inputValidation();
+    }
+    public CompassDirection inputValidation() {
+
+        if (input != null && !input.isBlank()) {
+            this.input = input.toUpperCase();
         } else {
-            this.compassDirection = " ";
+            this.input = " ";
         }
+        if (!input.isBlank()) {
+            extractCompassDirection();
+        }
+        return compassDirection;
+    }
+
+    private void extractCompassDirection() {
+
+            switch (input) {
+                case "N":
+                    compassDirection = CompassDirection.N;
+                    break;
+
+                case "S":
+                    compassDirection = CompassDirection.S;
+                    break;
+                case "E":
+                    compassDirection = CompassDirection.E;
+                    break;
+                case "W":
+                    compassDirection = CompassDirection.W;
+                    break;
+                default:
+                    System.out.println(input + " is an invalid Compass direction.");
+                    break;
+
+            }
     }
 }
