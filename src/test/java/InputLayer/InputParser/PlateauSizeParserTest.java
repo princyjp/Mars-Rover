d602eb6686f7c013aa3c5b6b1616a677e94470e1
princyjp <princy.philip@outlook.com>
2024-08-16 11:01:03 +0100

    test:InputLayer:InputParser:PlateauSizeParserTest:add test for valid input

--- src/test/java/InputLayer/InputParser/PlateauSizeParserTest.java
+++ src/test/java/InputLayer/InputParser/PlateauSizeParserTest.java
@@ -1,7 +1,27 @@
 package InputLayer.InputParser;
 
+import InputLayer.CompassDirection;
+import InputLayer.PlateauSize;
+import InputLayer.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauSizeParserTest {
+    @Test
+    @DisplayName("Return a valid plateausize for a valid input string")
+    void test_plateauSizeParser_validInput() {
+        String input = "1 2";
+
+        var result = new PlateauSizeParser(input).getPlateauSize();
+
+        assertAll(
+                () -> assertEquals(1, result.getLength()),
+                () -> assertEquals(2, result.getBreadth())
+        );
+
+    }
+
 
 }
